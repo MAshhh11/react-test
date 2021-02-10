b038eb207f31fe942b3573df0c170f023295c4bf
Marion Londero <maashhh11@gmail.com>
2021-02-10 11:08:54 +0100

    state & delete button

--- src/Table.js
+++ src/Table.js
@@ -6,6 +6,7 @@ const TableHeader = () => {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     )
@@ -16,6 +17,9 @@ const TableHeader = () => {
         <tr key={index}>
           <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
         </tr>
       )
     })
@@ -23,17 +27,28 @@ const TableHeader = () => {
     return <tbody>{rows}</tbody>
   }
 
-class Table extends Component {
-    render() {
-      const {characterData} = this.props
+  const Table = (props) => {
+    const {characterData, removeCharacter} = props
   
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={characterData} />
-        </table>
-      )
-    }
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+      </table>
+    )
   }
+
+// class Table extends Component {
+//     render() {
+//       const {characterData} = this.props
+  
+//       return (
+//         <table>
+//           <TableHeader />
+//           <TableBody characterData={characterData} />
+//         </table>
+//       )
+//     }
+//   }
 
 export default Table
